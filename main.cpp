b041713c4ce66d38143dd4fc3f97d14a6d0326ff
phoebe <phoebe@PHOEBE-PC>
2024-01-27 15:40:48 -0800

    Window set-up

--- main.cpp
+++ main.cpp
@@ -1,2 +1,61 @@
+#include <iostream>
+
+#include <glad/glad.h> // OpenGL function loader
 #include <GLFW/glfw3.h>
-#include <glad/glad.h>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+int main() {
+	glfwInit();
+
+	// Configure GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create window object
+	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window); // Make this the main context on current thread
+
+	// Initialize GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Set viewport dimensions
+	glViewport(0, 0, 800, 600); // In pixels
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // Adjust dimensions on window resize
+
+	// Render loop
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+
+		// Clear color buffer
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+	// Close window on escape
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
