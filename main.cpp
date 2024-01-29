375eff89f5d68528030e87af35b25d8e7c8c7687
phoebe <phoebe@PHOEBE-PC>
2024-01-28 19:43:33 -0800

    Create vertex buffer object and shader program object

--- main.cpp
+++ main.cpp
@@ -3,59 +3,129 @@
 #include <glad/glad.h> // OpenGL function loader
 #include <GLFW/glfw3.h>
 
+const char *vertexShaderSource = "#version 460 core\n"
+                                 "layout (location = 0) in vec3 pos;\n"
+                                 "void main() {\n"
+                                     "gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);\n"
+                                 "}\0";
+
+const char *fragmentShaderSource = "#version 460 core\n"
+                                   "out vec4 color;\n"
+                                   "void main() {\n"
+                                       "color = vec4(1.0f, 0.5f, 0.2f, 1.0f)\n"
+                                   "}\0";
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
 int main() {
-	glfwInit();
+    glfwInit();
 
-	// Configure GLFW
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Configure GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Create window object
-	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-	if (window == NULL) {
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window); // Make this the main context on current thread
+    // Create window object
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window); // Make this the main context on current thread
 
-	// Initialize GLAD
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
+    // Initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
-	// Set viewport dimensions
-	glViewport(0, 0, 800, 600); // In pixels
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // Adjust dimensions on window resize
+    // Set viewport dimensions
+    glViewport(0, 0, 800, 600); // In pixels
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // Adjust dimensions on window resize
 
-	// Render loop
-	while (!glfwWindowShouldClose(window)) {
-		processInput(window);
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
 
-		// Clear color buffer
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+    // Upload vertex data to GPU
+    unsigned int vertexBufferObject;
+    glGenBuffers(1, &vertexBufferObject);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // One write, many reads
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+    // Compile vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER); // Create empty shader object
+    glShaderSource(vertexShader, 1, &vertexShaderSource,NULL);
+    glCompileShader(vertexShader);
 
-	glfwTerminate();
-	return 0;
+    // Check for compilation error
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success); // Query shader for information
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "Vertex shader compilation failed: " << infoLog << std::endl;
+    }
+
+    // Compile fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Check for compilation error
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "Fragment shader compilation failed: " << infoLog << std::endl;
+    }
+
+    // Create shader program object and link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Check for link error
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "Shader linking failed: " << infoLog << std::endl;
+    }
+
+    // Activate shader program object
+    glUseProgram(shaderProgram);
+
+    // Shader object no longer needed
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Render loop
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        // Clear color buffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-	glViewport(0, 0, width, height);
+    glViewport(0, 0, width, height);
 }
 
 void processInput(GLFWwindow *window) {
-	// Close window on escape
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, true);
-	}
+    // Close window on escape
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
