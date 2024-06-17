7603920a7e23be1035e039366bc8b7833073616a
phoebe <phoebe@PHOEBE-PC>
2024-06-17 16:26:36 -0700

    Add vertex color attribute

--- main.cpp
+++ main.cpp
@@ -5,17 +5,18 @@
 
 const char *vertexShaderSource = "#version 460 core\n"
                                  "layout (location = 0) in vec3 pos;\n"
-                                 "out vec4 vertexColor;\n"
+                                 "layout (location = 1) in vec3 color;\n"
+                                 "out vec3 ourColor;\n"
                                  "void main() {\n"
                                      "gl_Position = vec4(pos, 1.0);\n"
-                                     "vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+                                     "ourColor = color;\n"
                                  "}\0";
 
 const char *fragmentShaderSource = "#version 460 core\n"
                                    "out vec4 fragColor;\n"
-                                   "uniform vec4 ourColor;\n"
+                                   "in vec3 ourColor;\n"
                                    "void main() {\n"
-                                       "fragColor = ourColor;\n"
+                                       "fragColor = vec4(ourColor, 1.0);\n"
                                    "}\0";
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
@@ -91,11 +92,11 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    // Position + color
     float vertices[] = {
-         0.5f,  0.5f, 0.0f, // top right
-         0.5f, -0.5f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f  // top left
+         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
+         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // top
     };
 
     unsigned int indices[] = {
@@ -115,14 +116,18 @@ int main() {
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // One write, many reads
 
     // Create, bind, and populate element buffer (for storing indices)
-    unsigned int elementBufferObject;
-    glGenBuffers(1, &elementBufferObject);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObject);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    //unsigned int elementBufferObject;
+    //glGenBuffers(1, &elementBufferObject);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObject);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // Configure and enable vertex buffer attributes
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    // Configure and enable position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0); // Enable attribute at location 0
+
+    // Configure and enable color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // Render loop
     while (!glfwWindowShouldClose(window)) {
@@ -135,15 +140,16 @@ int main() {
         // Activate shader program
         glUseProgram(shaderProgram);
 
-        // Update uniform color
-        float timeValue = glfwGetTime();
-        float greenValue = sin(timeValue) / 2.0f + 0.5f; // Vary from 0 to 1
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        //// Update uniform color
+        //float timeValue = glfwGetTime();
+        //float greenValue = sin(timeValue) / 2.0f + 0.5f; // Vary from 0 to 1
+        //int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        //glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
         // Draw rectangle from index buffer
         glBindVertexArray(vertexArrayObject); // Load VBO, EBO, and attributes
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
 
         // Swap buffers and poll IO events
