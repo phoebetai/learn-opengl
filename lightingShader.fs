33f3f3a7aa843b095cb899d743e23c2941ffeb21
phoebe <phoebe@PHOEBE-PC>
2024-09-08 13:50:02 -0700

    Add ambient, diffuse, and specular light components

--- lightingShader.fs
+++ lightingShader.fs
@@ -1,5 +1,13 @@
 #version 460 core
 
+struct Light {
+	vec3 position;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
 struct Material {
 	vec3 ambient;
 	vec3 diffuse;
@@ -12,29 +20,27 @@ out vec4 fragColor;
 in vec3 fragPos;
 in vec3 normal;
 
-uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
-
+uniform Light light;
 uniform Material material;
+uniform vec3 viewPos;
 
 void main() {
 	// Ambient light
-	vec3 ambient = material.ambient * lightColor;
+	vec3 ambient = light.ambient * material.ambient;
 
 	vec3 norm = normalize(normal);
-	vec3 lightDir = normalize(lightPos - fragPos);
+	vec3 lightDir = normalize(light.position - fragPos);
 
 	// Diffuse light
 	float diff = max(dot(norm, lightDir), 0.0); // Clamp to maximum of 90 degrees between two vectors
-	vec3 diffuse = diff * material.diffuse * lightColor;
+	vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
 	vec3 viewDir = normalize(viewPos - fragPos);
 	vec3 reflectDir = reflect(-lightDir, norm); // Reflected light vector
 
 	// Specular light
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess); // The smaller the angle between view and reflection, the stronger the specular light
-	vec3 specular = spec * material.specular * lightColor;
+	vec3 specular = light.specular * (spec * material.specular);
 
 	vec3 result = ambient + diffuse + specular;
 	fragColor = vec4(result, 1.0);
--- main.cpp
+++ main.cpp
@@ -163,15 +163,21 @@ int main() {
 
         //  Activate lighting shader
         lightingShader.use();
-        lightingShader.setVec3("lightColor",  1.0f, 1.0f, 1.0f);
-        lightingShader.setVec3("lightPos", lightPos);
-        lightingShader.setVec3("viewPos", camera.position);
+
+        // Set light parameters
+        lightingShader.setVec3("light.position", lightPos);
+        lightingShader.setVec3("light.ambient",  0.2f, 0.2f, 0.2f);
+        lightingShader.setVec3("light.diffuse",  0.5f, 0.5f, 0.5f);
+        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
 
         // Set material parameters
         lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
         lightingShader.setFloat("material.shininess", 32.0f);
+
+        // Set camera position
+        lightingShader.setVec3("viewPos", camera.position);
 
         // Pass projection transform to shader
         glm::mat4 projection = glm::perspective(glm::radians(camera.zoom), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
